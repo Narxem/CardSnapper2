fbc4a7755da81fc11681dc6283da137e59ff7e32
Narxem <adrien.agez@hotmail.fr>
2017-01-24 21:58:47 +0100

    Hello World !

---
+++ CardSnapper/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CardSnapper.Controllers
+{
+    public class HomeController : Controller {
+        // GET: Home
+        public ActionResult Index(string id) { 
+            if (string.IsNullOrEmpty(id))
+                return View("Error");
+            ViewData["nom"] = id;
+            return View();
+        }
+    }
+}
