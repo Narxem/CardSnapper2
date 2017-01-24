35f717fe7bd81b2d16e3981cd7adeca95481bfb5
Narxem <adrien.agez@hotmail.fr>
2017-01-25 00:42:24 +0100

    Add models

---
+++ CardSnapper/Models/BDDContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace CardSnapper.Models {
+    public class BddContext : DbContext {
+        public DbSet<User> users { get; set; }
+        public DbSet<Card> cards { get; set; }
+    }
+}
---
+++ CardSnapper/Models/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CardSnapper.Models {
+    public class Card {
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public int rarity { get; set; }
+        public string imageURL { get; set; }
+    }
+}
