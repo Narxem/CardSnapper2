1bf42ffff194bd9d990db85805e98710b9379037
Haelia <sarahwissocq@free.fr>
2017-01-27 19:05:46 +0100

    Modif view

---
+++ CardSnapper/Controllers/CardsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CardSnapper.Controllers
+{
+    public class CardsController : Controller
+    {
+
+        public ActionResult AllCards()
+        {
+            return View();
+        }
+
+        public ActionResult OpenBooster() {
+            return View();
+        }
+    }
+}
