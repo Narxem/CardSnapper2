25289ed2d96ee6d19bac143866fdac4d682e9229
Narxem <adrien.agez@hotmail.fr>
2017-01-27 20:25:38 +0100

    Add Card DAL

---
+++ CardSnapper/Models/CardDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CardSnapper.Models {
+    public class CardDal {
+        private BddContext db = new BddContext();
+        private Random rand = new Random();
+        public List<Card> getAllCards() {
+            return db.cards.ToList();
+        }
+
+        public Card getRandomCard() {
+            List<Card> cards = getAllCards();
+            return cards[rand.Next(0, cards.Count)];
+        }
+    }
+}
